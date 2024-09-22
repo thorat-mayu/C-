0d2d3f914f3b0dd2c9f22d1a92d53e54b8010549
thorat-mayu <178452253+thorat-mayu@users.noreply.github.com>
2024-09-22 23:33:39 +0530

    largest number program.cpp

---
+++ largest number program.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+int b;
+int c;
+cout <<"Put any three numbers: "<<endl;
+cin>>a;
+cin>>b;
+cin>>c;
+if(a>=b && a>=c)
+{
+cout<<"The Largest number is: "<<a;
+}
+if(b>=a && b>=c)
+{
+cout<<"The Largest number is: "<<b;
+}
+if(c>=a && c>=b)
+{
+cout<<"The Largest number is: "<<c;
+}
+return 0;
+}
+
+Output:
+
+Put any three numbers: 9 4 265
+The Largest number is: 265
