8099c3e06d66fafba7f73a7b342fdf643031edf2
thorat-mayu <178452253+thorat-mayu@users.noreply.github.com>
2024-09-22 23:30:09 +0530

    Even odd c++ program.cpp

---
+++ Even odd.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a;
+cout << "Enter the number: ";
+cin >> a;
+/* logic */
+if (a % 2 == 0) {
+cout << "The given number is EVEN" << endl;
+} else {
+cout << "The given number is ODD" << endl;
+}
+
+return 0;
+
+} 
+
+Output:
+
+Enter the number: 7
+The given number is ODD
+
